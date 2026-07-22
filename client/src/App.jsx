b687887e8d783cff60b9dc82737f4d75152a114e
Nikki <nikki@Nikkis-Mac.local>
2026-07-23 03:04:14 +0530

    feat : Create Dashboard Component

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+// function App() {
+//   return (
+//     <h1>Student Management System</h1>
+//   );
+// }
+
+// export default App;
+
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <Dashboard />
+  );
+}
+
+export default App;
