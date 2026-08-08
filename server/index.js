520e20f8a538f32e06420c6354d500719dff6715
Nikki <nikki@Nikkis-Mac.local>
2026-08-08 19:49:35 +0530

    feat: connect React frontend with students API

--- server/index.js
+++ server/index.js
@@ -1,17 +1,50 @@
-// const http = require("http");
+// // const http = require("http");
 
-// const server = http.createServer((req, res) => {
-//   res.write("Hello from Node");
-//   res.end();
+// // const server = http.createServer((req, res) => {
+// //   res.write("Hello from Node");
+// //   res.end();
+// // });
+
+// // server.listen(8000, () => {
+// //   console.log("Node server running on port 8000");
+// // });
+
+// // const express = require("express");
+//  const cors = require("cors");
+
+// //const app = express();
+// app.use(cors());
+
+// app.get("/", (req, res) => {
+//   res.send("Student Management System Backend Running 🚀");
 // });
 
-// server.listen(8000, () => {
-//   console.log("Node server running on port 8000");
+// app.get("/api/students", (req, res) => {
+//   res.json([
+//     {
+//       id: 1,
+//       name: "Nikki",
+//       roll: 101,
+//       course: "CSE",
+//     },
+//     {
+//       id: 2,
+//       name: "Rahul",
+//       roll: 102,
+//       course: "IT",
+//     },
+//   ]);
 // });
 
+// app.listen(8000, () => {
+//   console.log("Server running on port 8000");
+// });
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Student Management System Backend Running 🚀");
