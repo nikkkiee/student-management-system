afeab136deb6cab5c6291a1b53dedc0a75178abf
Nikki <nikki@Nikkis-Mac.local>
2026-08-11 21:12:14 +0530

    feat: add pages structure and react router

--- server/index.js
+++ server/index.js
@@ -110,12 +110,21 @@
 
 
 
+// const express = require("express");
+// const mysql = require("mysql2");
+
+// const app = express();
+
+// app.use(express.json());
 const express = require("express");
 const mysql = require("mysql2");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 
 const db = mysql.createConnection({
   host: "localhost",
